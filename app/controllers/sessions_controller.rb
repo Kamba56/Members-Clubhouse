3dfdd919c0eb064a21bfacb5c32147b780e669f1
Toma John Musa <tomajohn316@yahoo.com>
2023-04-10 09:50:12 +0100

    Fix linters

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,20 @@
 class SessionsController < ApplicationController
   def new() end
-    def create
-      user = User.find_by_email(params[:email])
 
-      if user && user.authenticate(params[:password])
-        session[:user_id] = user.user_id
-        redirect_to root_url, notice: "Logged in!"
-      else
-        flash.now.alert = "Invalid email or password"
-        render :new
-      end
-    end
+  def create
+    user = User.find_by_email(params[:email])
 
-    def destroy
-      session[:user_id] = nil
-      redirect_to root_url, notice: "Logged out!"
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.user_id
+      redirect_to root_url, notice: 'Logged in!'
+    else
+      flash.now.alert = 'Invalid email or password'
+      render :new
     end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: 'Logged out!'
+  end
 end
