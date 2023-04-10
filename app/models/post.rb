2605df25b578e901b981e9e7d11dc3d197555838
Toma John Musa <tomajohn316@yahoo.com>
2023-04-10 10:15:23 +0100

    Add associations between users and posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  belongs_to :user, dependent: :destroy
+
+  validates :text, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
 
+  has_many :posts
+
   attr_accessible :email, :password, :password_confirmation
 
   validates_uniqueness_of :email
