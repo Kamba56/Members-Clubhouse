aba62536cbd4de4e7b1d6c449e78228b7e4f7b7a
Toma John Musa <tomajohn316@yahoo.com>
2023-04-11 08:55:22 +0100

    Add signup form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :posts, only: %i[new create index]
+  resources :users, only: %i[new create]
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
