59153fa5b4c985d5b3ef46599048c0fa5e9e3d20
Toma John Musa <tomajohn316@yahoo.com>
2023-04-11 10:58:31 +0100

    Add login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :posts, only: %i[new create index destroy]
   resources :users, only: %i[new create]
+  resources :sessions, only: %i[create]
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
