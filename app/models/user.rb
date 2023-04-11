a8b9a6b815158b2eaa3fb275b7c0d56b6b6832c2
Toma John Musa <tomajohn316@yahoo.com>
2023-04-11 10:39:57 +0100

    Add delete Post functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
-  has_many :posts
+  has_many :posts, dependent: :delete_all
 
   # attr_accessible :email, :password, :password_confirmation
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :posts, only: %i[new create index]
+  resources :posts, only: %i[new create index destroy]
   resources :users, only: %i[new create]
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
